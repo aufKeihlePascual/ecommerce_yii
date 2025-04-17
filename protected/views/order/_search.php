f42b4c179443e1afa19fbd7b942b00dcfd7cb1b2
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2025-04-18 00:59:28 +0800

    Auto-generated by crud generator

---
+++ protected/views/order/_search.php
@@ -0,0 +1,50 @@
+<?php
+/* @var $this OrderController */
+/* @var $model Order */
+/* @var $form CActiveForm */
+?>
+
+<div class="wide form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'action'=>Yii::app()->createUrl($this->route),
+	'method'=>'get',
+)); ?>
+
+	<div class="row">
+		<?php echo $form->label($model,'id'); ?>
+		<?php echo $form->textField($model,'id'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'user_id'); ?>
+		<?php echo $form->textField($model,'user_id'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'cart_id'); ?>
+		<?php echo $form->textField($model,'cart_id'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'total'); ?>
+		<?php echo $form->textField($model,'total',array('size'=>10,'maxlength'=>10)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'status'); ?>
+		<?php echo $form->textField($model,'status',array('size'=>9,'maxlength'=>9)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'created_at'); ?>
+		<?php echo $form->textField($model,'created_at'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Search'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- search-form -->
