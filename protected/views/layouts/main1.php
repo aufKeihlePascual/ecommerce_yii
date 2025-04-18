63bc925cd0e51561ce556d31b3655de4ab48386b
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2025-04-18 17:01:37 +0800

    Updated code for main1.php to view other pages after clicking the navbar links. Previous version was only redirecting but not showing pages

--- protected/views/layouts/main1.php
+++ protected/views/layouts/main1.php
@@ -7,8 +7,10 @@
 
 	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+	
 	<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/style.css">
-	<script src="https://kit.fontawesome.com/4e3a20099e.js" crossorigin="anonymous"></script>
+	<!-- <script src="https://kit.fontawesome.com/4e3a20099e.js" crossorigin="anonymous"></script> -->
+	<script src="https://kit.fontawesome.com/4e3a20099e.js"></script>
 
 	<?php
 	// Prevent Yii from loading default CSS
@@ -52,19 +54,47 @@
 	<?php endif; ?>
 
 	<!-- PAGE CONTENT -->
-	<?php if (isset($this->breadcrumbs)): ?>
-		<div class="container py-3">
+	<div class="container py-5">
+		<?php if (isset($this->breadcrumbs)): ?>
 			<?php $this->widget('zii.widgets.CBreadcrumbs', array(
 				'links' => $this->breadcrumbs,
 			)); ?>
-		</div>
-	<?php endif; ?>
+		<?php endif; ?>
 
-	<?php echo $content; ?>
+		<?php echo $content; ?>
+	</div>
 
 	<!-- Footer -->
 	<footer class="bg-black text-secondary py-5 mt-5">
 		<div class="container">
+			<div class="row">
+				<div class="col-md-4 mb-4">
+					<h5 class="text-white">Collections</h5>
+					<ul class="list-unstyled">
+						<li><a href="#" class="footer-link">All Products</a></li>
+						<li><a href="#" class="footer-link">Group Buys</a></li>
+						<li><a href="#" class="footer-link">What's New</a></li>
+					</ul>
+				</div>
+				<div class="col-md-4 mb-4">
+					<h5 class="text-white">Contact</h5>
+					<p class="small">📞 09123456789<br>📧 info@innotech.com</p>
+					<div class="mt-2">
+						<a href="#" class="me-2 text-light"><i class="bi bi-facebook"></i></a>
+						<a href="#" class="me-2 text-light"><i class="bi bi-instagram"></i></a>
+						<a href="#" class="text-light"><i class="bi bi-discord"></i></a>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<h5 class="text-white">Newsletter</h5>
+					<p class="small">Get exclusive updates and deals.</p>
+					<form class="d-flex">
+						<input type="email" class="form-control form-control-sm me-2" placeholder="Your email">
+						<button class="btn btn-success btn-sm">Subscribe</button>
+					</form>
+				</div>
+			</div>
+			<hr class="border-secondary my-4">
 			<p class="text-center small mb-0">© <?php echo date('Y'); ?> InnoTech. All rights reserved.</p>
 		</div>
 	</footer>
