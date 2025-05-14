9106a944275fffc22cd0513d55374c537c682989
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2025-05-14 11:29:12 +0800

    Updated access rules for CartController and CategoryController

--- protected/controllers/CartController.php
+++ protected/controllers/CartController.php
@@ -32,7 +32,7 @@ class CartController extends Controller
 				'users'=>array('*'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update', 'testStripeKey'),
+				'actions'=>array('testStripeKey'),
 				'users'=>array('@'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
