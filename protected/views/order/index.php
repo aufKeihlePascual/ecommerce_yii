d9441b9b600f2819d4e8eddf08b5c119a0805a6e
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2025-05-14 12:05:10 +0800

    Removed admin sidebar from order/index, supposedly viewed only by user

--- protected/views/order/index.php
+++ protected/views/order/index.php
@@ -20,24 +20,6 @@ $this->breadcrumbs = array('Orders');
     </div>
 
 	<div class="order-page d-flex">
-		<aside class="sidebar">
-            <div class="filter-section">
-                <h4>Actions</h4>
-                <ul class="menu-links">
-                    <li>
-                        <a href="<?php echo CHtml::normalizeUrl(array('order/create')); ?>">
-                            <i class="fa fa-plus-circle"></i> Create Order
-                        </a>
-                    </li>
-                    <li>
-                        <a href="<?php echo CHtml::normalizeUrl(array('order/admin')); ?>">
-                            <i class="fa fa-cogs"></i> Manage Orders
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </aside>
-
 		<div class="order-table-wrapper">
 			<table class="order-table">
 				<thead>
