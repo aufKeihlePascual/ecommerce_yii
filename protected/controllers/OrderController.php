3f32f8f378d29fa43243c4c5c2048fcf61298342
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2025-05-14 12:30:09 +0800

    Added adminIndex for separate views for admin, index for login users, and deleted blank stripe.php file

--- protected/controllers/OrderController.php
+++ protected/controllers/OrderController.php
@@ -28,11 +28,11 @@ class OrderController extends Controller
 	{
 		return array(
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('view','create','update', 'stripeOrders'),
+				'actions'=>array('index', 'view','create','update', 'stripeOrders'),
 				'users'=>array('@'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('index','admin','delete'),
+				'actions'=>array('adminIndex','admin','delete'),
 				'users'=>array('admin'),
 			),
 			array('deny',  // deny all users
