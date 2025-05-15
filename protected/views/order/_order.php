b5a5dabefffb4fb0bb64563a47018afc0b4f246e
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2025-05-16 02:22:27 +0800

    _order.php button changed from Form POST to AJAX

--- protected/views/order/_order.php
+++ protected/views/order/_order.php
@@ -17,7 +17,9 @@
     <td>
         <a href="<?php echo CHtml::normalizeUrl(['order/viewOrderDetails', 'session_id' => $data->id]); ?>" class="view-btn">View</a>
          <form action="<?php echo CHtml::normalizeUrl(['payment/markAsShipped', 'session_id' => $data->id]); ?>" method="post" style="margin-top: 5px;">
-            <button type="submit" class="normal ship-btn" onclick="return confirm('Mark this order as shipped?');">Ship</button>
+            <button type="button" class="normal ship-btn"
+                data-session-id="<?php echo $data->id; ?>"
+                onclick="markAsShipped(this)">Ship</button>
         </form>
     </td>
 </tr>
